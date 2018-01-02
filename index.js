81b834b8592eaf4d10fc384d6d5ae2ec8979362d
Subhajit Sahu <wolfram77@gmail.com>
2018-01-02 21:35:22 +0530

    :snowman: fetching all properties

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const cp = require('child_process');
 const cheerio = require('cheerio');
 const httpStatus = require('http-status');
 const chalk = require('chalk');
-const arange = require('array-arange');
 
 
 // I. global variables
@@ -20,13 +19,12 @@ var verbose = false;
 const logSill = (msg) => { if(verbose) console.log(chalk.gray(msg)); };
 const logVerb = (msg) => { if(verbose) console.log(chalk.yellowBright(msg)); };
 const logErr = (msg) => { if(verbose) console.log(chalk.redBright(msg)); };
-const logTxt = (msg) => { if(verbose) console.log(msg); };
 
 
-function text(elm) {
+function text(elm, $) {
   // 1. get text in element
-  var chd = elm.children();
-  return chd.length>0? chd.text():elm.text();
+  if(!elm.childNodes.length) return $(elm).text();
+  return $(elm.firstChild).text();
 };
 
 function nameParts(z, str, id) {
@@ -51,7 +49,7 @@ function headerParts(z, $, elm) {
 function bodyParts(z, $, elm, vali) {
   // 1. get details from body
   var tds = $(elm).find('td');
-  var name = text($(tds[1])).trim();
+  var name = text(tds[1], $).trim();
   var unit = $(tds[2]).text().trim();
   var value = $(tds[vali]).text().trim();
   z[name] = `${value} ${unit}`;
@@ -63,13 +61,13 @@ const request = (path) => new Promise((fres, frej) => {
   var opt = {method: 'GET', hostname: 'ndb.nal.usda.gov', path, headers};
   logSill(`> GET https://${opt.hostname}${path}`);
   var req = https.request(opt, (res) => {
-    var dat = '';
     res.setEncoding('utf8');
-    logSill(`< ${res.statusCode} ${httpStatus[res.statusCode]}`);
+    var dat = '', code = res.statusCode, status = httpStatus[code];
+    logSill(`< https://${opt.hostname}${path} : ${code} ${status}`);
     res.on('data', (chu) => dat += chu);
     res.on('end', () => {
-      if(res.statusCode>=200 && res.statusCode<300) fres(dat);
-      else frej(new Error(res.statusCode+' '+httpStatus[res.statusCode]));
+      if(code>=200 && code<300) fres(dat);
+      else frej(new Error(code+' '+status));
     });
   });
   req.on('error', (e) => frej(e));
@@ -95,8 +93,9 @@ module.exports = usdaNdb;
 
 // IV. command-line
 const fetch = (err, id) => usdaNdb(id).then((dat) => {
-  logTxt(`${id}: ${dat['Number']}, ${dat['Name']}`);
-  if(output!=null) output.write(JSON.stringify(dat)+os.EOL);
+  logVerb(`${id}: ${dat['Number']}, ${dat['Name']} - ${Object.keys(dat).length} properties`);
+  if(output==null) console.log(JSON.stringify(dat));
+  else output.write(JSON.stringify(dat)+os.EOL);
 }, (e) => {
   logErr(`${id}: ${e.message}`);
   err.push(id);
@@ -105,7 +104,7 @@ const fetch = (err, id) => usdaNdb(id).then((dat) => {
 const fetchall = (ids, tim) => new Promise((fres) => {
   var i = 0, I = ids.length, con = 0, pro = [], err = [];
   var tmr = setInterval(() => {
-    if(i<I) return con<connections? pro[i] = (con++ && fetch(err, i++).then(() => con--)):null;
+    if(i<I) return con<connections? pro[i] = (con++ && fetch(err, ids[i++]).then(() => con--)):null;
     Promise.all(pro).then(() => fres(err));
     clearInterval(tmr);
   }, tim);
@@ -123,9 +122,9 @@ const run = (ids) => new Promise((fres) => {
 
 if(require.main===module) {
   // 1. process arguments
-  var values = [];
-  for(var i=0, I=A.length; i<I; i++) {
-    if(A[i]==='-o' || A[i]==='--output') output = A[++i];
+  var values = [], job = [];
+  for(var i=2, I=A.length; i<I; i++) {
+    if(A[i]==='-o' || A[i]==='--output') output = fs.createWriteStream(A[++i]);
     else if(A[i]==='-c' || A[i]==='--connections') connections = parseInt(A[++i], 10);
     else if(A[i]==='-t' || A[i]==='--timegap') timegap = parseInt(A[++i], 10);
     else if(A[i]==='-r' || A[i]==='--retries') retries = parseInt(A[++i], 10);
@@ -133,14 +132,18 @@ if(require.main===module) {
     else if(A[i]==='--help') return cp.execSync(`less ${__dirname}/README.md`, {stdio: [0, 1, 2]});
     else values.push(A[i]);
   }
+  // 2. run job
   var start = parseInt(values[0], 10)||0, stop = parseInt(values[1], 10)||start+1;
   logVerb(`Fetching ${start} -> ${stop}:`);
   logVerb(`- output file: ${output}`);
   logVerb(`- connections: ${connections}`);
-  logVerb(`- timegap:     ${connections} ms`);
-  logVerb(`- retries:     ${connections}`);
-  run(arange(start, stop)).then((err) => {
-    logVerb(`${start} -> ${stop} done, ${err.length} failed.`);
-    console.error('FAILED:', err);
+  logVerb(`- timegap:     ${timegap} ms`);
+  logVerb(`- retries:     ${retries}`);
+  for(var i=start; i<stop; i++)
+    job[i-start] = i.toString();
+  run(job).then((err) => {
+    logVerb(`${start} -> ${stop} done; ${job.length-err.length} passed, ${err.length} failed.`);
+    if(err.length>0) console.error(err.length, err);
+    if(output!=null) output.end();
   });
 }
