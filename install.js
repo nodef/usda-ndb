284e2afbbb59386cd03daa4611b86964cb986783
Subhajit Sahu <wolfram77@gmail.com>
2018-01-02 21:53:09 +0530

    :snowman: delete index.txt

--- install.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-const os = require('os');
-const cp = require('child_process');
-
-if(os.EOL==='\n') cp.execSync(
-  `tr -d '\r' <index.sh >index.cmd && `+
-  'chmod +x index.cmd'
-);
